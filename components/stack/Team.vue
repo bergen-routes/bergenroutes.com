c9e89aa906826506a5350d7a37aba2edf4cde369
Mattpacc <mattypacc@gmail.com>
2025-06-04 14:04:42 -0400

    Update Team.vue

--- components/stack/Team.vue
+++ components/stack/Team.vue
@@ -8,7 +8,7 @@ const members = [
     }, {
         name: "Matthew Paccione",
         icon: "/images/team/matthew_paccione.jpg",
-        title: "Project Manager",
+        title: "Product Manager",
         url: "https://www.linkedin.com/in/matthew-paccione-373980240/"
     }, {
         name: "Lisa Nam",
